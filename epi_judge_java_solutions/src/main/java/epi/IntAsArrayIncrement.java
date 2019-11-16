57f2a517d30558db5dcecb086836568a36bd444c
s0umyadeep <Soumyadeep@bitbucket.org>
2019-11-16 22:59:26 +1100

    Increment array

--- epi_judge_java_solutions/src/main/java/epi/IntAsArrayIncrement.java
+++ epi_judge_java_solutions/src/main/java/epi/IntAsArrayIncrement.java
@@ -8,22 +8,38 @@ import java.util.List;
 public class IntAsArrayIncrement {
 
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
-  public static List<Integer> plusOne(List<Integer> A) {
+  public static List<Integer> plusOne(List<Integer> a) {
 
-    int n = A.size() - 1;
-    A.set(n, A.get(n) + 1);
-    for (int i = n; i > 0 && A.get(i) == 10; --i) {
-      A.set(i, 0);
-      A.set(i - 1, A.get(i - 1) + 1);
+    if (a.size() <= 0) {
+      return a;
     }
-    if (A.get(0) == 10) {
-      // There is a carry-out, so we need one more digit to store the result.
-      // A slick way to do this is to append a 0 at the end of the array,
-      // and update the first entry to 1.
-      A.set(0, 1);
-      A.add(0);
+
+    int n = a.size() - 1;
+    a.set(n, a.get(n) + 1);
+    for (int i = n; i > 0 && a.get(i) == 10; i--) {
+      a.set(i, 0);
+      a.set(i-1, a.get(i-1)+1);
     }
-    return A;
+
+    if(a.get(0) == 10) {
+      a.set(0,1);
+      a.add(0);
+    }
+
+//    int n = a.size() - 1;
+//    a.set(n, a.get(n) + 1);
+//    for (int i = n; i > 0 && a.get(i) == 10; --i) {
+//      a.set(i, 0);
+//      a.set(i - 1, a.get(i - 1) + 1);
+//    }
+//    if (a.get(0) == 10) {
+//      // There is a carry-out, so we need one more digit to store the result.
+//      // A slick way to do this is to append a 0 at the end of the array,
+//      // and update the first entry to 1.
+//      a.set(0, 1);
+//      a.add(0);
+//    }
+    return a;
   }
 
   public static void main(String[] args) {
